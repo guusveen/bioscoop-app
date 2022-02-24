4b2b8186149b02956b946159c3e2974b97e38f4e
Guus Veen <guusveen>
2022-02-24 18:32:48 +0100

    commit met alle files

---
+++ Bioscoop App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bioscoop_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
